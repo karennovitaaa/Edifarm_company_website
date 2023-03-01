86a51f980f343d0a0db4363230390be36d087e70
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2023-03-01 23:03:27 +0700

    mentahan

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', function() {
     return view('login');
 });
+Route::get('/register', function() {
+    return view('register');
+});
+Route::get('/postingan', function() {
+    return view('postingan');
+});
