b23eaaca17eb5c19085086d6680f0edae800a8dd
WIZ-H4L <wishal572@gmail.com>
2023-03-07 12:14:17 +0700

    login,regis

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>SocialV | Responsive Bootstrap 4 Admin Dashboard Template</title>
+      <title>Edifarm</title>
       
       <link rel="shortcut icon" href="images/favicon.ico" />
       <link rel="stylesheet" href="css/libs.min.css">
@@ -38,21 +38,21 @@
             <div class="row no-gutters">
                 <div class="col-md-6 text-center pt-5">
                     <div class="sign-in-detail text-white">
-                        <a class="sign-in-logo mb-5" href="#"><img src="images/logo-full.png" class="img-fluid" alt="logo"></a>
+                        <a class="sign-in-logo mb-5" href="#"><img src="images/logo1.png" class="img-fluid" alt="logo"></a>
                         <div class="sign-slider overflow-hidden ">
                             <ul  class="swiper-wrapper list-inline m-0 p-0 ">
                                 <li class="swiper-slide">
-                                    <img src="images/login/1.png" class="img-fluid mb-4" alt="logo">
+                                    <img src="images/login/tani1.jpeg" class="img-fluid mb-4" alt="logo">
                                     <h4 class="mb-1 text-white">Find new friends</h4>
                                     <p>It is a long established fact that a reader will be distracted by the readable content.</p>
                                 </li>
                                 <li class="swiper-slide">
-                                    <img src="images/login/2.png" class="img-fluid mb-4" alt="logo"> 
+                                    <img src="images/login/tani2.jpeg" class="img-fluid mb-4" alt="logo"> 
                                     <h4 class="mb-1 text-white">Connect with the world</h4>
                                     <p>It is a long established fact that a reader will be distracted by the readable content.</p>
                                 </li>
                                 <li class="swiper-slide">
-                                    <img src="images/login/3.png" class="img-fluid mb-4" alt="logo">
+                                    <img src="images/login/tani3.jpg" class="img-fluid mb-4" alt="logo">
                                     <h4 class="mb-1 text-white">Create new events</h4>
                                     <p>It is a long established fact that a reader will be distracted by the readable content.</p>
                                 </li>
@@ -85,11 +85,11 @@
                                 <button type="submit" class="btn btn-primary float-end">Sign Up</button>
                             </div>
                             <div class="sign-info">
-                                <span class="dark-color d-inline-block line-height-2">Already Have Account ? <a href="sign-in.html">Log In</a></span>
+                                <span class="dark-color d-inline-block line-height-2">Already Have Account ? <a href="login">Log In</a></span>
                                 <ul class="iq-social-media">
-                                    <li><a href="#"><i class="ri-facebook-box-line"></i></a></li>
-                                    <li><a href="#"><i class="ri-twitter-line"></i></a></li>
-                                    <li><a href="#"><i class="ri-instagram-line"></i></a></li>
+                                    <li><a href="https://www.facebook.com/profile.php?id=100088522217152"><i class="ri-facebook-box-line"></i></a></li>
+                                    <li><a href="https://www.twitter.com/@Edifarm21"><i class="ri-twitter-line"></i></a></li>
+                                    <li><a href="https://www.instagram.com/edifarmm_"><i class="ri-instagram-line"></i></a></li>
                                 </ul>
                             </div>
                         </form>
