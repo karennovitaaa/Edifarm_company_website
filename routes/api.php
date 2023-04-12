709beb35fd9a528879ad908da6867da095991490
aditiyagilang <aditiyagilang031@gmail.com>
2023-04-12 22:36:10 +0700

    halo

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\API\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('post', [AuthController::class, 'post']);
-Route::post('getpost', [AuthController::class, 'getpost']);
+Route::get('getpost', [AuthController::class, 'getpost']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
