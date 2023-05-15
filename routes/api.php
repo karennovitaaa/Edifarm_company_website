f8c1aa248f32f8330bf1397c4c989b7c7180726c
andruchristo27 <100926838+andruchristo27@users.noreply.github.com>
2023-05-15 20:31:54 +0700

    cek

--- routes/api.php
+++ routes/api.php
@@ -36,5 +36,4 @@ Route::post('deleteStatusSession', [AuthController::class, 'deleteStatusSession'
 Route::post('getpostuse', [AuthController::class, 'getpostuse']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-<<<<<<< HEAD
 });
